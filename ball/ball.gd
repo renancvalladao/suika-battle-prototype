c90afe752c388efde05b0b92f54b471a0a6161c6
Renan Valladão <renancvalladao@gmail.com>
2023-12-27 22:00:18 -0300

    Creating Ball scene

---
+++ ball/ball.gd
@@ -0,0 +1,31 @@
+extends RigidBody2D
+
+class_name Ball
+
+@onready var sprite = $Sprite
+@onready var icon = $Icon
+
+var config: Dictionary = BallsManager.BALLS[0]
+var exploded: bool = false
+
+func _ready():
+	$Icon.texture = config.icon
+	$Icon.scale *= config.size 
+	$Sprite.texture = config.sprite
+	$Sprite.scale *= config.size
+	$CollisionShape2D.scale *= config.size
+
+func set_configuration(new_config: Dictionary):
+	config = new_config
+
+func explode():
+	exploded = true
+	queue_free()
+
+func _on_body_entered(body):
+	if config.tier == BallsManager.BALLS.size():
+		return
+	if body is Ball && body.config.size == config.size && !exploded:
+		explode()
+		body.explode()
+		BallsManager.ball_exploded.emit(position, body.position, config.tier)
