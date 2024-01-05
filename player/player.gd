38041126a99f4d39c4cb156f8d063ead1693c7e8
Renan Valladão <renancvalladao@gmail.com>
2024-01-05 20:38:39 -0300

    Adding health and mana bar label

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var progress_bar = $ProgressBar
+@onready var health_bar = $HealthBar
+@onready var health_label = $HealthBar/HealthLabel
 @onready var shield_label = $TextureRect/ShieldLabel
 
 var hp = 100
@@ -10,7 +11,7 @@ func _ready():
 	SignalManager.player_damaged.connect(player_damaged)
 	SignalManager.health_gained.connect(health_gained)
 	SignalManager.shield_gained.connect(shield_gained)
-	progress_bar.value = hp
+	update_health_ui(hp)
 	shield_label.text = str(shield)
 
 func player_damaged(damage: int) -> void:
@@ -25,14 +26,19 @@ func player_damaged(damage: int) -> void:
 		hp = 0
 		SignalManager.on_game_over.emit()
 	shield_label.text = str(shield)
-	progress_bar.value = hp
+	update_health_ui(hp)
 
 func health_gained(health: int) -> void:
 	hp += health
 	if hp > 100:
 		hp = 100
-	progress_bar.value = hp
+	update_health_ui(hp)
 
 func shield_gained(amount: int) -> void:
 	shield += amount
 	shield_label.text = str(shield)
+
+func update_health_ui(new_hp: int) -> void:
+	health_bar.value = hp
+	health_label.text = str("%s/100" % hp)
+	
