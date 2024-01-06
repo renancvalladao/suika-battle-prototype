9de6536a6d9900bcad0707c910bce52e4fa7cc0e
Renan Valladão <renancvalladao@gmail.com>
2024-01-05 21:14:02 -0300

    Updating mana on ball explosion

--- player/player.gd
+++ player/player.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 @onready var health_bar = $HealthBar
 @onready var health_label = $HealthBar/HealthLabel
+@onready var mana_bar = $ManaBar
+@onready var mana_label = $ManaBar/ManaLabel
 @onready var shield_label = $TextureRect/ShieldLabel
 
 var hp = 100
+var mana = 0
 var shield = 0
 
 func _ready():
 	SignalManager.player_damaged.connect(player_damaged)
 	SignalManager.health_gained.connect(health_gained)
 	SignalManager.shield_gained.connect(shield_gained)
+	BallsManager.ball_exploded.connect(ball_exploded)
 	update_health_ui(hp)
+	update_mana_ui(mana)
 	shield_label.text = str(shield)
 
 func player_damaged(damage: int) -> void:
@@ -41,4 +46,15 @@ func shield_gained(amount: int) -> void:
 func update_health_ui(new_hp: int) -> void:
 	health_bar.value = hp
 	health_label.text = str("%s/100" % hp)
-	
+
+func update_mana_ui(new_mana: int) -> void:
+	mana_bar.value = mana
+	mana_label.text = str("%s/100" % mana)
+
+func ball_exploded(_first_pos: Vector2, _second_pos: Vector2, tier: int):
+	if BallsManager.turn == 1:
+			return
+	mana += tier * 5
+	if mana > 100:
+		mana = 100
+	update_mana_ui(mana)
