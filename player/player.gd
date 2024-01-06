8d0e5342b2647dff80e81a3315a74493b2eb344f
Renan Valladão <renancvalladao@gmail.com>
2024-01-05 22:29:02 -0300

    Adding attack, shield and heal button

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+const ATTACK_MANA = 10
+const SHIELD_MANA = 15
+const HEAL_MANA = 20
+
 @onready var health_bar = $HealthBar
 @onready var health_label = $HealthBar/HealthLabel
 @onready var mana_bar = $ManaBar
 @onready var mana_label = $ManaBar/ManaLabel
 @onready var shield_label = $TextureRect/ShieldLabel
 @onready var chain_explosion_timer = $ChainExplosionTimer
+@onready var attack_button = $VBoxContainer/AttackButton
+@onready var shield_button = $VBoxContainer/ShieldButton
+@onready var heal_button = $VBoxContainer/HealButton
 
 var hp = 100
 var mana = 0
@@ -17,9 +24,24 @@ func _ready():
 	SignalManager.health_gained.connect(health_gained)
 	SignalManager.shield_gained.connect(shield_gained)
 	BallsManager.ball_exploded.connect(ball_exploded)
+	BallsManager.turn_finished.connect(turn_finished)
+	SignalManager.turn_started.connect(turn_started)
+	attack_button.pressed.connect(on_attack_pressed)
+	shield_button.pressed.connect(on_shield_pressed)
+	heal_button.pressed.connect(on_health_pressed)
 	update_health_ui(hp)
 	update_mana_ui(mana)
 	shield_label.text = str(shield)
+
+func turn_finished():
+	attack_button.disabled = true
+	shield_button.disabled = true
+	heal_button.disabled = true
+
+func turn_started():
+	attack_button.disabled = false || mana < ATTACK_MANA
+	shield_button.disabled = false || mana < SHIELD_MANA
+	heal_button.disabled = false || mana < HEAL_MANA
 
 func player_damaged(damage: int) -> void:
 	shield -= damage
@@ -50,6 +72,9 @@ func update_health_ui(new_hp: int) -> void:
 	health_label.text = str("%s/100" % hp)
 
 func update_mana_ui(new_mana: int) -> void:
+	attack_button.disabled = mana < ATTACK_MANA
+	shield_button.disabled = mana < SHIELD_MANA
+	heal_button.disabled = mana < HEAL_MANA
 	mana_bar.value = mana
 	mana_label.text = str("%s/100" % mana)
 
@@ -67,3 +92,24 @@ func ball_exploded(_first_pos: Vector2, _second_pos: Vector2, tier: int):
 
 func _on_chain_explosion_timer_timeout():
 	explosion_chain = false
+
+func on_attack_pressed():
+	if mana < ATTACK_MANA:
+		return
+	mana -= ATTACK_MANA
+	update_mana_ui(mana)
+	SignalManager.enemy_damaged.emit(10)
+
+func on_shield_pressed():
+	if mana < SHIELD_MANA:
+		return
+	mana -= SHIELD_MANA
+	update_mana_ui(mana)
+	SignalManager.shield_gained.emit(10)
+
+func on_health_pressed():
+	if mana < HEAL_MANA:
+		return
+	mana -= HEAL_MANA
+	update_mana_ui(mana)
+	SignalManager.health_gained.emit(15)
