61804ff6146c4ae1fa1a15097ff1bf527259cde0
Renan Valladão <renancvalladao@gmail.com>
2024-01-08 12:50:34 -0300

    Adding bomb explosion range

--- bomb/bomb.gd
+++ bomb/bomb.gd
@@ -1,7 +1,13 @@
 extends Ball
 
 @onready var explosion_timer = $ExplosionTimer
+@onready var explosion_area = $ExplosionArea
+
 var will_explode = false
+
+func _ready():
+	super._ready()
+	$ExplosionArea/ExplosionShape.scale *= config.size
 
 func _on_body_entered(body):
 	super._on_body_entered(body)
@@ -11,7 +17,7 @@ func _on_body_entered(body):
 	explosion_timer.start()
 
 func _on_explosion_timer_timeout():
-	for body in get_colliding_bodies():
+	for body in explosion_area.get_overlapping_bodies():
 		if body is Ball:
 			body.queue_free()
 	queue_free()
