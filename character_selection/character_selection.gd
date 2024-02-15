8acc3838b19b0c0268d9a992ff057bbb01d35eef
Renan Valladão <renancvalladao@gmail.com>
2024-02-14 21:26:41 -0300

    Adding action quantity config

--- character_selection/character_selection.gd
+++ character_selection/character_selection.gd
@@ -1,9 +1,11 @@
 extends Control
 
 @onready var balls_quantity = $BallsQuantity
+@onready var actions_quantity = $ActionsQuantity
 
 func _process(_delta):
 	balls_quantity.text = "%s" % GameManager.max_balls
+	actions_quantity.text = "%s" % GameManager.max_actions
 
 func _on_minus_button_pressed():
 	GameManager.max_balls -= 1
@@ -12,3 +14,11 @@ func _on_minus_button_pressed():
 
 func _on_plus_button_pressed():
 	GameManager.max_balls += 1
+
+func _on_action_minus_pressed():
+	GameManager.max_actions -= 1
+	if GameManager.max_actions <= 1:
+		GameManager.max_actions = 1
+
+func _on_action_plus_pressed():
+	GameManager.max_actions += 1
