49c6f7b27560cc5f22c1798cbc2e97311ed03561
Renan Valladão <renancvalladao@gmail.com>
2024-10-21 23:51:52 -0300

    ghost ball damage

--- ghost_ball/ghost_ball.gd
+++ ghost_ball/ghost_ball.gd
@@ -1,4 +1,10 @@
 extends Ball
 
+var ATTACK_DAMAGE = 5
+
+func _on_body_entered(body):
+	if body is EnemyBall:
+		body.enemy_damaged(ATTACK_DAMAGE * config.tier, true)
+
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
